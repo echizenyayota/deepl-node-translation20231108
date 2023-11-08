83c4a60ab1a31ae55d3d95858978dcac15d041c7
echizenyayota <virtualmisso30@gmail.com>
2023-11-08 17:50:40 +0900

    make the text area in the TextBox component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Arrows from "./components/Arrows";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
+import TextBox from "./components/TextBox";
+
 const App = () => {
   return (
     <div className="app">
-
+      <TextBox style="input"/>
+      <TextBox style="output"/>
     </div>
   );
 }
