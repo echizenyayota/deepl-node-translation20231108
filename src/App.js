0fdb43427d2077176748b91cc51462c476360a25
echizenyayota <virtualmisso30@gmail.com>
2023-11-10 18:32:46 +0900

    switch between inputLanguage and outputLanguage, using the Arrow

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import Arrows from "./components/Arrows";
 import Button from "./components/Button";
 import Modal from "./components/Modal";
 import TextBox from "./components/TextBox";
+import { useState } from "react";
+
 
 const App = () => {
+
+  const [inputLanguage, setInputLanguage] = useState("English(en)");
+  const [outputLanguage, setOutputLanguage] = useState("Japanese(ja)");
+
+  const handleClick = () => {
+    setInputLanguage(outputLanguage);
+    setOutputLanguage(inputLanguage);
+  }
+
   return (
     <div className="app">
-      <TextBox style="input"/>
-      <TextBox style="output"/>
+      <TextBox
+        selectedLanguage={inputLanguage} 
+        style="input"
+      />
+      <div className="arrow-container" onClick={handleClick}>
+        <Arrows />
+      </div>
+      <TextBox
+        selectedLanguage={outputLanguage}
+        style="output"
+      />
     </div>
   );
 }
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,10 +1,12 @@
 import SelectDropDown from "./SelectDropDown";
 
 
-const TextBox = ({style}) => {
+const TextBox = ({style, selectedLanguage}) => {
   return (
     <div className={style}>
-      <SelectDropDown />
+      <SelectDropDown 
+        selectedLanguage={selectedLanguage}
+      />
       <textarea 
         placeholder={style === "input" ? "Enter Text" : "translation"}
         disabled={style === "output"}
