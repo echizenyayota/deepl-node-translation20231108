4721a52983670e98ad14e74ee09d21df02ff2790
echizenyayota <virtualmisso30@gmail.com>
2023-11-11 21:31:24 +0900

    show all the languages to translate on a screen

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,17 @@ const app = express();
 app.use(cors());
 
 app.listen(PORT, () => console.log("Server running on PORT " + PORT));
+
+app.get("/languages", async(req, res) => {
+  const authKey = process.env.DEEPL_API_KEY;
+  const translator = new deepl.Translator(authKey);
+  const sourceLanguage = await translator.getSourceLanguages();
+
+  try {
+    res.status(200).json(sourceLanguage.map(lang => `${lang.name}(${lang.code})`));
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({message: err});
+  }
+
+});
