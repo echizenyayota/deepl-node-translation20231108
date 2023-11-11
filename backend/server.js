97b3465fe9a6d49b01a11116f74c40d9f31405c9
echizenyayota <virtualmisso30@gmail.com>
2023-11-11 21:17:54 +0900

    make an env file to save Deepl's API key

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const PORT = 8000;
 const axios = require("axios").default;
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
