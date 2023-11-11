a58e56bdc5e9321237d0279545b76d84eda32b20
echizenyayota <virtualmisso30@gmail.com>
2023-11-11 22:02:46 +0900

    bring the langauges to translate from the backend to frontend

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import Arrows from "./components/Arrows";
 import Button from "./components/Button";
 import Modal from "./components/Modal";
 import TextBox from "./components/TextBox";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
 
   const [inputLanguage, setInputLanguage] = useState("English(en)");
   const [outputLanguage, setOutputLanguage] = useState("Japanese(ja)");
   const [showModal, setShowModal] = useState(null);
+  const [languages, setLanguages] = useState(null);
+
+  const getLanguages = async() => {
+    const response = await axios.get("http://localhost:8000/languages");
+    setLanguages(response.data);
+  }
+
+  console.log("languages", languages);
+
+  useEffect(() => {
+    getLanguages();
+  }, []);
 
   const handleClick = () => {
     setInputLanguage(outputLanguage);
